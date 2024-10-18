3f4e9051e4bb57e2d496ff2ce70dfbcfb94f7d70
Jamal H <Vega.cf16@gmail.com>
2024-10-18 11:29:25 -0400

    Simplified friction further
    No need to check if the velocity is 0, the friction will end up as zero in that case anyways

--- RaylibTest/ForceSystem.cpp
+++ RaylibTest/ForceSystem.cpp
@@ -20,26 +20,16 @@ void ForceSystem::Update(Scene* scene, std::vector<Force>& forceComponents)
 		Vector2 totalForce = Vector2Add(force.internalForce, force.externalForce);
 		float totalAngularForce = force.internalAngularForce + force.externalAngularForce;
 
-		// initialize friction to 0
-		Vector2 frictionForce = { 0.0f, 0.0f };
-		float angularFrictionForce = 0.0f;
-
-		// dont find friction if velocity is roughly 0
-		if (Vector2Length(rigidBody.velocity) != 0)
-			frictionForce = Vector2Scale(Vector2Normalize(rigidBody.velocity), -rigidBody.frictionCoefficient * gravity * rigidBody.mass);
-
-		// dont find angular friction if angular velocity is roughly 0
-		if (rigidBody.angularVelocity != 0)
-			angularFrictionForce = Clamp(rigidBody.angularVelocity, -1, 1) * -rigidBody.frictionCoefficient * rigidBody.angularVelocity;
+		// calculate friction
+		Vector2 frictionForce = Vector2Scale(Vector2Normalize(rigidBody.velocity), -rigidBody.frictionCoefficient * gravity * rigidBody.mass);
+		float angularFrictionForce = Clamp(rigidBody.angularVelocity, -1, 1) * -rigidBody.frictionCoefficient * rigidBody.angularVelocity;
 
 		// add friction
 		totalForce = Vector2Add(totalForce, frictionForce);
 		totalAngularForce += angularFrictionForce;
 
 		// f = ma so a = f/m
-		// linear acceleration
 		rigidBody.acceleration = Vector2Scale(totalForce, 1.0f / rigidBody.mass);
-		// angular acceleration
 		rigidBody.angularAcceleration = totalAngularForce / rigidBody.mass;
 	}
 }
