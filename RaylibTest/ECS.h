780e252183b648f16e3c2e70759b7a314aa0524d
Jamal H <Vega.cf16@gmail.com>
2024-10-18 22:59:59 -0400

    Included header file tree diagram

--- RaylibTest/ECS.h
+++ RaylibTest/ECS.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "Scene.h"
+
+
+/*
+*							ECS
+*							   |
+*						  SCENE ----> UNORDERED_MAP
+*						/			 \
+*			  ENTITY	         SYSTEMS ----> VECTOR
+*									/			   \
+*							 INPUT			  COMPONENTS
+*												/						\
+*										RAYLIB				RAYMATH
+*/
