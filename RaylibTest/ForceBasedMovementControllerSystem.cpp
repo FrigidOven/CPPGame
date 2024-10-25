12e800ca64d469ee4cc4712ac1989cac33316c5b
Jamal H <Vega.cf16@gmail.com>
2024-10-25 10:15:29 -0400

    Update ForceBasedMovementControllerSystem.cpp

--- RaylibTest/ForceBasedMovementControllerSystem.cpp
+++ RaylibTest/ForceBasedMovementControllerSystem.cpp
@@ -10,6 +10,71 @@ void ForceBasedMovementControllerSystem::Update(Scene* scene, std::vector<ForceB
 {
 	int requiredComponentsMask = 1 << Velocity::ID;
 
+
+
+
+
+	for (auto& forceBasedMovementController : forceBasedMovementControllers)
+	{
+		if ((scene->GetComponentMask(forceBasedMovementController.entity) & requiredComponentsMask) != requiredComponentsMask)
+			continue;
+
+		int entity = forceBasedMovementController.entity;
+
+		Vector2 upForce(0.0f, -forceBasedMovementController.force);
+		Vector2 leftForce(-forceBasedMovementController.force, 0.0f);
+		Vector2 downForce(0.0f, forceBasedMovementController.force);
+		Vector2 rightForce(forceBasedMovementController.force, 0.0f);
+
+		Vector2 previousForce(0.0f, 0.0f);
+		float frictionCoefficient = 0.0f;
+
+		bool wasMoving = scene->HasComponent<ForceBasedMovement>(entity);
+
+		if (wasMoving)
+			previousForce = scene->GetComponent<ForceBasedMovement>(entity).force;
+
+		if (wasMoving && scene->HasComponent<Friction>(entity))
+			frictionCoefficient = scene->GetComponent<Friction>(entity).coefficient;
+
+		bool wasMoving[4] =
+		{
+			Vector2Equals(previousForce, upForce),
+			Vector2Equals(previousForce, leftForce),
+			Vector2Equals(previousForce, downForce),
+			Vector2Equals(previousForce, rightForce)
+		};
+		bool isMoving[4] =
+		{
+			forceBasedMovementController.up.isActive,
+			forceBasedMovementController.left.isActive,
+			forceBasedMovementController.down.isActive,
+			forceBasedMovementController.right.isActive
+		};
+
+		for (int i = 0; i < 4; i++)
+		{
+			if (wasMoving[i] && !isMoving[i])
+				scene->RemoveComponent<ForceBasedMovement>(entity);
+
+
+
+		}
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 	for (auto& forceBasedMovementController : forceBasedMovementControllers)
 	{
 		if ((scene->GetComponentMask(forceBasedMovementController.entity) & requiredComponentsMask) != requiredComponentsMask)
